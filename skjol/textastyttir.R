a4028b4f058ec6346b985880fabbbebade2e99c0
harkanatta <valtyr@biopol.is>
2023-01-31 10:07:05 +0000

    baseurl

--- skjol/textastyttir.R
+++ skjol/textastyttir.R
@@ -1,2 +1,2 @@
-x <- 'Starfsmaður BioPol, Valtýr Sigurðsson, fór suður á Þingvelli í nýliðinni viku til að kanna möguleika á notkun neðansjávardróna við vöktun Þingvallavatns. Vöktunin er gerð af Náttúrufræðistofu Kópavogs en okkar hlutverk væri, ef af verður, að stýra kafbátnum við myndatökur á gróðurþekju botnsins. Lygnt Þingvallavatnið var þægilæg tilbreyting fyrir sjóveikan sjávarlíffræðing.'
+x <- 'Heiðrún Eiríksdóttir fyrrum starfsmaður BioPol sem starfar núna hjá Náttúrufræðistofnun á Akureyri fékk á dögunum útgefna ritrýnda grein í tímaritinu Marine Drugs. Rannsókn hennar fjallar um vöxt olíumyndandi örvera í æti framleiddu úr aukaafurðum sláturhúsa'
 ifelse(is.na(stringr::word(x, 1, 30)), x, stringr::word(x, 1, 30))
